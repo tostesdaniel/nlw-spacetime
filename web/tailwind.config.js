a16d9ecbd4ca3ac7edb2158422bba93b96abaffc
Daniel Tostes <ohtosttnz@gmail.com>
2023-05-18 15:39:55 -0300

    chore: create stripes backgroundSize

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -77,6 +77,9 @@ module.exports = {
         stripes:
           'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
       },
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
       blur: {
         full: '194px',
       },
